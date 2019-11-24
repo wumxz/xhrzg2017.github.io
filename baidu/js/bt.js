5f7a4edff377ad9815157425d1e086eacc7e6658
xhrzg2017 <xhrzg2017@gmail.com>
2019-11-24 10:20:13 +0800

    Signed-off-by: xhrzg2017 <xhrzg2017@gmail.com>

---
+++ baidu/js/bt.js
@@ -0,0 +1,19 @@
+﻿function bt(){
+	addt=document.getElementById("adddiv");
+	post=document.createElement("input");
+	post.setAttribute("type","text");
+	post.setAttribute("id","shihere");
+	post.setAttribute("name","shihere");
+	post.setAttribute("onKeyUp","vtq();")
+	post.setAttribute("placeholder","输入完成后F5刷新即可 B站@豚鼠萌君 微信公众号@豚鼠萌君")
+	
+	addt.appendChild(post);
+	
+	
+	
+	ppp=document.getElementById("p");
+	ppp.innerHTML="B站@豚鼠萌君";
+	
+	
+		
+	}
---
+++ baidu/js/bv.js
@@ -0,0 +1,29 @@
+﻿function bv(){
+	alert("如果设置背景视频失败 可去豚鼠公众号自行修改代码（有提示） 公众号@豚鼠极客");
+	bvbvb=document.getElementById("bvt");
+			xinbiao=document.createElement("source");
+			xinbiao.setAttribute("type","video/mp4");
+			xinbiao.setAttribute("src","niconico.mp4");
+			bvbvb.appendChild(xinbiao);
+	
+	
+	
+	
+	document.getElementById("wenjian").click();
+		filebg=document.getElementById("wenjian").files[0];
+		var reader=new FileReader();
+		reader.readAsDataURL(filebg);
+		reader.onload=function(){
+		var result = this.result;
+		 try {
+            localStorage.setItem("myBV", result);
+			bvbvb=document.getElementById("bvt");
+			xinbiao=document.createElement("source");
+			xinbiao.setAttribute("type","video/mp4");
+			xinbiao.setAttribute("src","niconico.mp4");
+			bvbvb.appendChild(xinbiao);
+        }catch (e) {
+            console.log("Storage failed: " + e);
+        }	
+		}
+	}	
